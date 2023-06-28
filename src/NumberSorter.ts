8423354f56f2abc0db7cf984796d8686018c4189
arsalanlal414 <arsalanlal414@gmail.com>
2023-06-28 23:08:16 +0500

    add some Bonus Features

--- src/NumberSorter.ts
+++ src/NumberSorter.ts
@@ -1,3 +1,74 @@
+// export class NumberSorter {
+//     private numbers: number[];
+  
+//     constructor(numbers: number[]) {
+//       this.numbers = numbers;
+//     }
+  
+//     // Sorts the numbers in descending order using merge sort algorithm.
+   
+//     sortDescending(): void {
+//       this.numbers = this.mergeSortDescending(this.numbers);
+//     }
+  
+//     // Recursively performs merge sort in descending order on an array.
+     
+//     private mergeSortDescending(arr: number[]): number[] {
+//       if (arr.length <= 1) {
+//         return arr;
+//       }
+  
+//       const mid = Math.floor(arr.length / 2);
+//       const left = arr.slice(0, mid);
+//       const right = arr.slice(mid);
+  
+//       return this.mergeDescending(
+//         this.mergeSortDescending(left),
+//         this.mergeSortDescending(right)
+//       );
+//     }
+  
+//     // Merges two arrays in descending order.
+
+//     private mergeDescending(left: number[], right: number[]): number[] {
+//       const merged: number[] = [];
+  
+//       let leftIndex = 0;
+//       let rightIndex = 0;
+  
+//       while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] > right[rightIndex]) {
+//           merged.push(left[leftIndex]);
+//           leftIndex++;
+//         } else {
+//           merged.push(right[rightIndex]);
+//           rightIndex++;
+//         }
+//       }
+  
+//       // Add the remaining elements from the left array
+//       while (leftIndex < left.length) {
+//         merged.push(left[leftIndex]);
+//         leftIndex++;
+//       }
+  
+//       // Add the remaining elements from the right array
+//       while (rightIndex < right.length) {
+//         merged.push(right[rightIndex]);
+//         rightIndex++;
+//       }
+  
+//       return merged;
+//     }
+  
+//     // Returns the sorted numbers array.
+     
+//     getSortedNumbers(): number[] {
+//       return this.numbers;
+//     }
+//   }
+  
+
 export class NumberSorter {
     private numbers: number[];
   
@@ -5,15 +76,18 @@ export class NumberSorter {
       this.numbers = numbers;
     }
   
+    // Sorts the numbers in ascending order using merge sort algorithm.
+    sortAscending(): void {
+      this.numbers = this.mergeSortAscending(this.numbers);
+    }
+  
     // Sorts the numbers in descending order using merge sort algorithm.
-   
     sortDescending(): void {
       this.numbers = this.mergeSortDescending(this.numbers);
     }
   
-    // Recursively performs merge sort in descending order on an array.
-     
-    private mergeSortDescending(arr: number[]): number[] {
+    // Recursively performs merge sort in ascending order on an array.
+    private mergeSortAscending(arr: number[]): number[] {
       if (arr.length <= 1) {
         return arr;
       }
@@ -22,22 +96,20 @@ export class NumberSorter {
       const left = arr.slice(0, mid);
       const right = arr.slice(mid);
   
-      return this.mergeDescending(
-        this.mergeSortDescending(left),
-        this.mergeSortDescending(right)
+      return this.mergeAscending(
+        this.mergeSortAscending(left),
+        this.mergeSortAscending(right)
       );
     }
   
-    // Merges two arrays in descending order.
-
-    private mergeDescending(left: number[], right: number[]): number[] {
+    // Merges two arrays in ascending order.
+    private mergeAscending(left: number[], right: number[]): number[] {
       const merged: number[] = [];
-  
       let leftIndex = 0;
       let rightIndex = 0;
   
       while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] > right[rightIndex]) {
+        if (left[leftIndex] <= right[rightIndex]) {
           merged.push(left[leftIndex]);
           leftIndex++;
         } else {
@@ -61,10 +133,56 @@ export class NumberSorter {
       return merged;
     }
   
-    // Returns the sorted numbers array.
-     
-    getSortedNumbers(): number[] {
-      return this.numbers;
+    // Recursively performs merge sort in descending order on an array.
+    private mergeSortDescending(arr: number[]): number[] {
+      if (arr.length <= 1) {
+        return arr;
     }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return this.mergeDescending(
+    this.mergeSortDescending(left),
+    this.mergeSortDescending(right)
+    );
+
+    }
+
+    // Merges two arrays in descending order.
+    private mergeDescending(left: number[], right: number[]): number[] {
+    const merged: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+  if (left[leftIndex] >= right[rightIndex]) {
+    merged.push(left[leftIndex]);
+    leftIndex++;
+  } else {
+    merged.push(right[rightIndex]);
+    rightIndex++;
   }
-  
+}
+
+// Add the remaining elements from the left array
+while (leftIndex < left.length) {
+  merged.push(left[leftIndex]);
+  leftIndex++;
+}
+
+// Add the remaining elements from the right array
+while (rightIndex < right.length) {
+  merged.push(right[rightIndex]);
+  rightIndex++;
+}
+
+return merged;
+
+}
+
+// Returns the sorted numbers array.
+getSortedNumbers(): number[] {
+return this.numbers;
+}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,91 @@
+// import fs from "fs";
+// import path from "path";
+// import readline from "readline";
+// import { NumberSorter } from "./NumberSorter";
+
+// function main() {
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+
+//   rl.question("Enter the input file name: ", (inputFilePath) => {
+//     rl.question("Enter the output file name: ", (outputFilePath) => {
+//       rl.close();
+
+//       try {
+//         const fileContent = fs.readFileSync(
+//           path.join(__dirname, inputFilePath),
+//           "utf-8"
+//         );
+//         const numbers: number[] = fileContent
+//           .split(", ")
+//           .map((num) => parseFloat(num)); // Use parseFloat to convert string to number
+
+//         const numberSorter: NumberSorter = new NumberSorter(numbers);
+//         numberSorter.sortDescending();
+
+//         const sortedNumbers: string = numberSorter.getSortedNumbers().join(", ");
+
+//         fs.writeFileSync(path.join(__dirname, outputFilePath), sortedNumbers);
+//         console.log("Sorting completed successfully!");
+//       } catch (error) {
+//         console.error("An error occurred:", error);
+//       }
+//     });
+//   });
+// }
+
+// main();
+
+
+
 import fs from "fs";
 import path from "path";
+import readline from "readline";
 import { NumberSorter } from "./NumberSorter";
 
 function main() {
-  const inputFilePath = "input.txt";
-  const outputFilePath = "output.txt";
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  try {
-    // Read the file content from the input file
-    const fileContent = fs.readFileSync(
-      path.join(__dirname, inputFilePath),
-      "utf-8"
-    );
+  rl.question("Enter the input file name: ", (inputFilePath) => {
+    rl.question("Enter the output file name: ", (outputFilePath) => {
+      rl.question("Choose the sort order (asc/desc): ", (sortOrder) => {
+        rl.close();
 
-    // Split the file content by comma and space
-    const numbers: number[] = fileContent
-      .split(", ")
-      .map((num) => parseFloat(num)); // Use parseFloat to convert string to number
+        try {
+          const fileContent = fs.readFileSync(
+            path.join(__dirname, inputFilePath),
+            "utf-8"
+          );
+          const numbers: number[] = fileContent
+            .split(", ")
+            .map((num) => parseFloat(num)); // Use parseFloat to convert string to number
 
-    // Create a NumberSorter object with the numbers array
-    const numberSorter: NumberSorter = new NumberSorter(numbers);
+          const numberSorter: NumberSorter = new NumberSorter(numbers);
 
-    // Sort the numbers in descending order
-    numberSorter.sortDescending();
+          if (sortOrder === "asc") {
+            numberSorter.sortAscending();
+          } else if (sortOrder === "desc") {
+            numberSorter.sortDescending();
+          } else {
+            console.error("Invalid sort order. Please choose 'asc' or 'desc'.");
+            return;
+          }
 
-    // Get the sorted numbers as a string
-    const sortedNumbers: string = numberSorter.getSortedNumbers().join(", ");
+          const sortedNumbers: string = numberSorter.getSortedNumbers().join(", ");
 
-    // Write the sorted numbers to the output file
-    fs.writeFileSync(path.join(__dirname, outputFilePath), sortedNumbers);
-
-    console.log("Sorting completed successfully!");
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+          fs.writeFileSync(path.join(__dirname, outputFilePath), sortedNumbers);
+          console.log("Sorting completed successfully!");
+        } catch (error) {
+          console.error("An error occurred:", error);
+        }
+      });
+    });
+  });
 }
 
 main();
