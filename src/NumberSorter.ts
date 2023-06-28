b5557f974a9cb0e8fa587f9f37b7deae95002d80
arsalanlal414 <arsalanlal414@gmail.com>
2023-06-28 13:23:09 +0500

    meeting all the initial requirements

---
+++ src/NumberSorter.ts
@@ -0,0 +1,70 @@
+export class NumberSorter {
+    private numbers: number[];
+  
+    constructor(numbers: number[]) {
+      this.numbers = numbers;
+    }
+  
+    // Sorts the numbers in descending order using merge sort algorithm.
+   
+    sortDescending(): void {
+      this.numbers = this.mergeSortDescending(this.numbers);
+    }
+  
+    // Recursively performs merge sort in descending order on an array.
+     
+    private mergeSortDescending(arr: number[]): number[] {
+      if (arr.length <= 1) {
+        return arr;
+      }
+  
+      const mid = Math.floor(arr.length / 2);
+      const left = arr.slice(0, mid);
+      const right = arr.slice(mid);
+  
+      return this.mergeDescending(
+        this.mergeSortDescending(left),
+        this.mergeSortDescending(right)
+      );
+    }
+  
+    // Merges two arrays in descending order.
+
+    private mergeDescending(left: number[], right: number[]): number[] {
+      const merged: number[] = [];
+  
+      let leftIndex = 0;
+      let rightIndex = 0;
+  
+      while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] > right[rightIndex]) {
+          merged.push(left[leftIndex]);
+          leftIndex++;
+        } else {
+          merged.push(right[rightIndex]);
+          rightIndex++;
+        }
+      }
+  
+      // Add the remaining elements from the left array
+      while (leftIndex < left.length) {
+        merged.push(left[leftIndex]);
+        leftIndex++;
+      }
+  
+      // Add the remaining elements from the right array
+      while (rightIndex < right.length) {
+        merged.push(right[rightIndex]);
+        rightIndex++;
+      }
+  
+      return merged;
+    }
+  
+    // Returns the sorted numbers array.
+     
+    getSortedNumbers(): number[] {
+      return this.numbers;
+    }
+  }
+  
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import fs from "fs";
 import path from "path";
+import { NumberSorter } from "./NumberSorter";
 
 function main() {
-  const file = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-  const [list] = file.split("\n");
+  const inputFilePath = "input.txt";
+  const outputFilePath = "output.txt";
 
-  const result = list;
+  try {
+    // Read the file content from the input file
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, inputFilePath),
+      "utf-8"
+    );
 
-  fs.writeFileSync(path.join(__dirname, "output.txt"), result.toString());
+    // Split the file content by comma and space
+    const numbers: number[] = fileContent
+      .split(", ")
+      .map((num) => parseFloat(num)); // Use parseFloat to convert string to number
+
+    // Create a NumberSorter object with the numbers array
+    const numberSorter: NumberSorter = new NumberSorter(numbers);
+
+    // Sort the numbers in descending order
+    numberSorter.sortDescending();
+
+    // Get the sorted numbers as a string
+    const sortedNumbers: string = numberSorter.getSortedNumbers().join(", ");
+
+    // Write the sorted numbers to the output file
+    fs.writeFileSync(path.join(__dirname, outputFilePath), sortedNumbers);
+
+    console.log("Sorting completed successfully!");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
+
 main();
